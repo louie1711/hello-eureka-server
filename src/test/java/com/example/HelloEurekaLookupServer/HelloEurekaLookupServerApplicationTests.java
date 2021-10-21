6a9b8b740cc1f47c1c7122bcc792cc14667c95bf
louie1711 <louis.moloney@gmail.com>
2021-10-21 17:52:28 +0100

    code for microservices tutorial

---
+++ src/test/java/com/example/HelloEurekaLookupServer/HelloEurekaLookupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloEurekaLookupServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEurekaLookupServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
